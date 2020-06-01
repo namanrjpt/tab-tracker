79fc4afa962b87ae2fb0cfa95c8249cca5f738d7
Naman Rajput <mail.namanrajput@gmail.com>
2020-06-02 01:51:50 +0530

    made the server side and introduced an endpoint

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
